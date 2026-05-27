a0704e34e1c09cfc819586b0bc94cd5847fddd06
Leoni Voulgaris <leonivou@gmail.com>
2026-05-27 16:32:24 +0100

    feat:make quadrants flex-align

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -2,7 +2,7 @@ function Circle({ id, fromQuadrant }) {
   return (
     <div draggable
       onDragStart={(e) => e.dataTransfer.setData("text/plain", JSON.stringify({ id, fromQuadrant }))}>
-      <svg width="100" height="100" viewBox="0 0 100 100">
+      <svg width="85" height="85" viewBox="0 0 100 100">
         <circle cx="50" cy="50" r="48" fill="white" stroke="black" strokeWidth="2"/>
       </svg>
     </div>
--- src/components/Quadrant.jsx
+++ src/components/Quadrant.jsx
@@ -1,12 +1,15 @@
 function Quadrant({ id, onDrop, shapes, component: ComponentType }){
   return (
-    <div className="rectangle" onDragOver={(e) => {e.preventDefault();}} onDrop={(e) => {const data = JSON.parse(e.dataTransfer.getData("text/plain"));
-      onDrop(data.id, data.fromQuadrant, id);
-
+    <div className="rectangle"
+    onDragOver={(e) => {e.preventDefault();}}
+    onDrop={(e) => {const data = JSON.parse(e.dataTransfer.getData("text/plain"));
+    onDrop(data.id, data.fromQuadrant, id);
     }}>
+
       {shapes.map((shapeId) => (
         <ComponentType key={shapeId} id={shapeId} fromQuadrant={id} />
       ))}
+
     </div>
   )
 }
