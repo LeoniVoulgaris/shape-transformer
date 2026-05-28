a6bbf858a8442ba9b5e01df6bce48f711ed06c47
Leoni Voulgaris <leonivou@gmail.com>
2026-05-28 10:44:24 +0100

    chore: changed .rectangle to .quadrant

--- src/components/Quadrant.jsx
+++ src/components/Quadrant.jsx
@@ -1,6 +1,6 @@
 function Quadrant({ id, onDrop, shapes, component: ComponentType }){
   return (
-    <div className="rectangle"
+    <div className="quadrant"
     /* preventDefault is required to allow onDrop, browsers block drops by default*/
     onDragOver={(e) => {e.preventDefault();}}
     onDrop={(e) => {const data = JSON.parse(e.dataTransfer.getData("text/plain"));
